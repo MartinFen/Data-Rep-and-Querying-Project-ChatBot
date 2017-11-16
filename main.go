6e23ea1d1bc4f8d64f9bf2418e4dbfc161dcd5b9
Martin Fennell <MartinFen@users.noreply.github.com>
2017-11-16 18:25:48 +0000

    Reading in file with data

--- main.go
+++ main.go
@@ -1,38 +1,58 @@
-/*	Author: Martin Fennell
-	Date: 2017
-*/
-
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
+type Response struct {
+	re        *regexp.Regexp
+	responses []string
+}
+
+func buildResponseList() []Response {
+	//empty struct
+	response := Response{}
+	//empty array of structs
+	allResponses := []Response{}
+	//read in a file
+	file, _ := os.Open("./data/patterns.dat")
+	//create scanner
+	scanner := bufio.NewScanner(file)
+	//loop through scanner until theres nothing left to scan
+	for scanner.Scan() {
+		//fmt.Println(scanner.Text())
+		patternStr := scanner.Text()
+		response.re = regexp.MustCompile(patternStr)
+		scanner.Scan() // move onto the next line which holds the answers
+		answersAsStr := scanner.Text()
+		//split responses up when a ; is in scope of scanner
+		answerList := strings.Split(answersAsStr, ";")
+		//add split responses
+		response.responses = answerList
+		allResponses = append(allResponses, response)
+	}
+	//fmt.Println(allResponses)
+	return allResponses //return list
+}
+
+//main func where it loops until the user quits
+
 func main() {
-
-	// Print a greeting to the user.
-	fmt.Println("Hello, I'm Eliza. How are you feeling today?")
-
-	// Keep reading user input and printing Eliza's response until the user types 'quit'.
+	fmt.Println("Hi my name is Eliza, whats yours?")
 	for reader := bufio.NewReader(os.Stdin); ; {
 		// Print user prompt.
 		fmt.Print("> ")
 		// Read user input.
-		userinput, _ := reader.ReadString('\n')
+		userInput, _ := reader.ReadString('\n')
 		// Trim the user input's end of line characters.
-		userinput = strings.Trim(userinput, "\r\n")
-		//hard coded output
-		output := "Nice to meet you"
-
-		// Generate and print Eliza's response.
-		fmt.Println(output)
-
-		// If the user input was quit, then quit.
-		// Note that Eliza gets to respond to quit before this happens.
-		if strings.Compare(strings.ToLower(strings.TrimSpace(userinput)), "quit") == 0 {
+		userInput = strings.Trim(userInput, "\r\n")
+		fmt.Println(buildResponseList())
+		//Quit program
+		if strings.Compare(strings.ToLower(strings.TrimSpace(userInput)), "quit") == 0 {
 			fmt.Println("Bye.")
 			break
 		}
